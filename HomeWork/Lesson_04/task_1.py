5ce76d6e70c7f449caac48f6c72ed021b6e37a60
Елизавета Кузьмина <ekuzmina@TNEWS.local>
2023-07-21 15:46:17 +0300

    Задача 1 домашнего задания 4 коррективы

--- HomeWork/Lesson_04/task_1.py
+++ HomeWork/Lesson_04/task_1.py
@@ -1,15 +1,15 @@
 # ✔ Напишите функцию для транспонирования матрицы
 
 
-def transpose(matrix):
+def transpose(num_matrix):
     """Функция для транспонирования матрицы"""
     res = []
-    row = len(matrix)
-    colum = len(matrix[0])
+    row = len(num_matrix)
+    colum = len(num_matrix[0])
     for j in range(colum):
         tmp = []
         for i in range(row):
-            tmp = tmp + [matrix[i][j]]
+            tmp = tmp + [num_matrix[i][j]]
         res = res + [tmp]
     return res
 
@@ -18,8 +18,7 @@ matrix = [[1, 2, 4, 5],
           [6, 7, 8, 9],
           [0, 11, 12, 13]]
 print("ИСХОДНАЯ МАТРИЦА: \n\t" + '\n\t'.join(list(map(str, matrix))))
-print("ТРАНСПОНИРОВАНИЕ МАТРИЦЫ С ПОМОЩЬЮ ФУНКЦИИ: \n\t" +'\n\t'.join(list(map(str, transpose(matrix)))))
+print("ТРАНСПОНИРОВАНИЕ МАТРИЦЫ С ПОМОЩЬЮ ФУНКЦИИ: \n\t" + '\n\t'.join(list(map(str, transpose(matrix)))))
 # решение с использованием zip() (zip() возвращает элементы в кортеже -> в списке!)
 print("ТРАНСПОНИРОВАНИЕ МАТРИЦЫ С ПОМОЩЬЮ ФУНКЦИИ zip():\n\t" +
       '\n\t'.join(list(map(str, [list(line) for line in zip(*matrix)]))))
-
