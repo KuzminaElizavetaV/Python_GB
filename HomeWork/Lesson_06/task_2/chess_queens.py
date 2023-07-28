1632447a6fb687f8a0461fcc72503d63aa86c192
Елизавета Кузьмина <ekuzmina@TNEWS.local>
2023-07-28 16:06:26 +0300

    Задача 3 домашнего задания 6

--- HomeWork/Lesson_06/task_2/chess_queens.py
+++ HomeWork/Lesson_06/task_2/chess_queens.py
@@ -1,5 +1,9 @@
+from random import randint
+__all__ = ["print_check_result", "arrange_queens"]
+
+
 def check_queens(pos) -> bool:
-    """Определяет, есть ли среди ферзей пара бьющих друг друга."""
+    """Определяет, есть ли среди ферзей пара бьющих друг друга"""
     beats = True
     for i in range(len(pos)):
         row, col = pos[i]
@@ -15,6 +19,16 @@ def print_check_result(pos):
         print("Ферзи не бьют друг друга")
     else:
         print("Ферзи бьют друг друга")
+
+
+def arrange_queens():
+    """Выводит 4 успешных расстановки ферзей"""
+    combinations = 4
+    while combinations > 0:
+        pos = list((i, randint(1, 8)) for i in range(1, 8))
+        if check_queens(pos):
+            print(*pos)
+            combinations -= 1
 
 
 if __name__ == "__main__":
